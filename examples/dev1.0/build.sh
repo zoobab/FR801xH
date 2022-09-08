19d987f37327d490ed4078387d76793f7b199cfd
Benjamin Henrion <zoobab@gmail.com>
2022-09-08 20:46:32 +0200

    add build.sh

---
+++ examples/dev1.0/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eo pipefail
+# only some examples compile...
+for i in ble_simple_peripheral ble_multi_role; do
+        echo "===== Building $i... ====="
+        cd $i/gcc
+        make clean
+        make
+        cd ../..
+        echo "===== Built $i ! ======"
+done
